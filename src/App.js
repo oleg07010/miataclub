e6278a2a30d076b35a03a24b8ee154f6ad728eeb
Oleg <42892503+oleg07010@users.noreply.github.com>
2024-10-20 10:57:47 -0400

    New pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Events from './pages/Events';
+import Forums from './pages/Forums';
+import Members from './pages/Members';
+import Contacts from './pages/Contacts';
+import News from './pages/News';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/forums" element={<Forums />} />
+        <Route path="/members" element={<Members />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/news" element={<News />} />
+      </Routes>
     </div>
   );
 }
